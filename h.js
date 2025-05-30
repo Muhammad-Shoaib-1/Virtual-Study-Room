827ff4eed6386d8b593c2a86762605efea964fca
Muhammad Shoaib <muhammad.shoaib.priv@gmail.com>
2025-05-30 11:58:40 +0500

    Initial commit

---
+++ h.js
@@ -0,0 +1,16 @@
+ 
+
+ console.log("helow word")
+
+ var a = prompt("enter your number")
+    
+       
+ var istrue =confirm("are you sure leave this page and blast your computer")
+ if(istrue){
+
+console.log("blasting your computer")
+ }
+ else {
+    console.log("not blasting your computer")
+ }
+ console.log(" your number is "+ a)
